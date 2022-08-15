69e46ea9b747878731e238fc459f2b011e412800
Saheed Adekunle Raji <rsa.oluwaseun@gmail.com>
2022-08-15 14:20:04 +0100

    Graphical construction of the relationship between stream order and stream number and stream length

---
+++ Horton's law graphs.R
@@ -0,0 +1,43 @@
+##Graphs to validate Horton's laws of drainage basin
+
+library(readxl)
+library(ggplot2)
+library(ggpmisc)
+
+data1 <- read_excel("~/Research 2021/Tropical Geomorphometry Study Group/Paper Two/Horton graphs.xlsx", 
+              sheet = "Stream number")
+
+View(data1)
+
+data("data1")
+
+names(data1)
+
+# assembling a single label with equation and R2
+
+my.formula = y ~ poly(x,1)
+
+G1 <- ggplot(data = data1, aes(x = Stream_order, y = Stream_number)) + geom_point() +
+  geom_smooth(formula = my.formula, method = "lm", se = T, level = 0.5) + 
+  stat_poly_eq(formula = my.formula, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+               parse = T, size = 4, colour = "blue") + 
+  labs(x = "Stream Order", y = "Stream Number (Log)")
+
+
+data2 <- read_excel("~/Research 2021/Tropical Geomorphometry Study Group/Paper Two/Horton graphs.xlsx", 
+                    sheet = "Length")
+
+
+
+my.formula = y ~ poly(x,1)
+
+G2 <- ggplot(data = data2, aes(x = Stream_order, y = Stream_length)) + geom_point() +
+  geom_smooth(formula = my.formula, method = "lm", se = T, level = 0.5) + 
+  stat_poly_eq(formula = my.formula, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+               parse = T, size = 4, colour = "blue") + 
+  labs(x = "Stream Order", y = "Stream Length (Log)")
+
+G2                              
+
+
+G1 + G2
